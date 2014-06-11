167d5e5a84b236ad07fc2112358adeacbd670c25
u0jing <u9jing@gmail.com>
2014-06-11 11:23:16 +0800

    修改onstep

--- src/main/scala/com/asiainfo/ocdc/streaming/impl/ExampleStep.scala
+++ src/main/scala/com/asiainfo/ocdc/streaming/impl/ExampleStep.scala
@@ -5,29 +5,51 @@ import org.apache.spark.streaming.dstream.DStream
 import com.asiainfo.ocdc.streaming.tools.HbaseTable
 import org.apache.hadoop.hbase.util.Bytes
 import scala.xml.Node
+import com.asiainfo.ocdc.streaming.tools.HbaseTable._
 
 class ExampleStep extends StreamingStep{
 
   def onStep(step:Node,inStream:DStream[Array[String]]):DStream[Array[String]]={
-    val input = (step \ "input").text.toString.split(",")
-    val output = (step \ "output").text.toString.split(",")
-    val table = (step \ "HBaseTable").text.toString
-    val col = (step \ "HBaseCol").text.toString
-    val key = (step \ "HBaseKey").text.toString
-    val where = (step \ "where").text.toString
+
+    val delim = ","
+    var result = inStream
+    val Input = (step \ "Input").text.toString.split(delim)
+    val HBaseTable = (step \ "HBaseTable").text.toString
+    val HBaseKey = (step \ "HBaseKey").text.toString.split(delim)
+    val output = (step \ "output").text.toString.split(delim)
+    val HBaseValue = (step \ "HBaseValue").text.toString
+    val where = (step \ "where").text.toString // table1.city!=CITY_ID
+
     var handle = inStream.map(x=>{
-      val imap = (0 to x.length-1).map(i=>(input(i),x(i)))
-      val row = HbaseTable.getValue(table,key)
-      col.split(",").map(c=>{
-        (table+"."+c,Bytes.toString(row.getValue(Bytes.toBytes("f1"), Bytes.toBytes(c))))
-      })++imap
+      var IMap =(0 to x.length-1).map(i=>(Input(i),x(i)))
+      var key = ""
+      for(arg <- HBaseKey){
+        val item =  IMap.toMap
+        key +=item(arg)
+      }
+      val HBaseRow  = getValue(HBaseTable,key)
+      if(HBaseRow != null){
+        IMap ++= HBaseValue.split(delim).map(c=>{
+          (HBaseTable+"."+c,Bytes.toString(HBaseRow.getValue(Bytes.toBytes("F"), Bytes.toBytes(c))))
+        })
+      }
+      else{
+        IMap ++= HBaseValue.split(delim).map(c=>{
+          (HBaseTable+"."+c," ")  })
+      }
+      IMap
     })
-    if(!where.equals("")){
-      handle = handle.filter(getResult(where,_))
+
+    if(where != null){
+      handle = handle.filter(x=>{
+        getResult(where, x.toArray)
+      })
     }
-    handle.map(x=>{
+    result  = handle.map(x=>{
+      //如果input output相同的字段完全相同，说明不需要规整数据，不做map
       val item = x.toMap
       (0 to output.length-1).map(i=>item.getOrElse(output(i),output(i))).toArray
     })
+    result
   }
 }
