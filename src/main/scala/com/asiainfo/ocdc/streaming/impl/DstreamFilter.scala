6c8244368f10b6b44811435cc7c786ca79572f1d
u0jing <u9jing@gmail.com>
2014-06-11 17:17:40 +0800

    过滤

---
+++ src/main/scala/com/asiainfo/ocdc/streaming/impl/DstreamFilter.scala
@@ -0,0 +1,55 @@
+package com.asiainfo.ocdc.streaming.impl
+
+import com.asiainfo.ocdc.streaming._
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.hadoop.hbase.util.Bytes
+import scala.xml.Node
+import com.asiainfo.ocdc.streaming.tools.HbaseTable._
+
+class DStreamFilter extends StreamingStep{
+
+  def onStep(step:Node,inStream:DStream[Array[String]]):DStream[Array[String]]={
+
+    val delim = ","
+    var result = inStream
+    val Input = (step \ "Input").text.toString.split(delim)
+    val HBaseTable = (step \ "HBaseTable").text.toString
+    val HBaseKey = (step \ "HBaseKey").text.toString.split(delim)
+    val output = (step \ "output").text.toString.split(delim)
+    val HBaseValue = (step \ "HBaseValue").text.toString
+    val where = (step \ "where").text.toString // table1.city!=CITY_ID
+
+    var handle = inStream.map(x=>{
+      var IMap =(0 to x.length-1).map(i=>(Input(i),x(i)))
+      var key = ""
+      for(arg <- HBaseKey){
+        val item =  IMap.toMap
+        key +=item(arg)
+      }
+      val HBaseRow  = getValue(HBaseTable,key)
+      if(HBaseRow != null){
+        IMap ++= HBaseValue.split(delim).map(c=>{
+          (HBaseTable+"."+c,Bytes.toString(HBaseRow.getValue(Bytes.toBytes("F"), Bytes.toBytes(c))))
+        })
+      }
+      else{
+        IMap ++= HBaseValue.split(delim).map(c=>{
+          (HBaseTable+"."+c," ")  })
+      }
+      IMap
+    })
+
+    if(where != null){
+      handle = handle.filter(x=>{
+        getResult(where, x.toArray)
+      })
+    }
+    result  = handle.map(x=>{
+      //如果input output相同的字段完全相同，说明不需要规整数据，不做map
+      val item = x.toMap
+      (0 to output.length-1).map(i=>item.getOrElse(output(i),output(i))).toArray
+    })
+    result
+
+  }
+}
