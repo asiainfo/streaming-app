b8887baa0c61336ebf013ffe98f49cf1de42a5b1
Leolh <leosandylh@gmail.com>
2014-06-12 10:04:48 +0800

    resolve imported twice class StringBuilder in the same scope

--- src/main/scala/com/asiainfo/ocdc/streaming/impl/KafkaSource.scala
+++ src/main/scala/com/asiainfo/ocdc/streaming/impl/KafkaSource.scala
@@ -4,8 +4,7 @@ import com.asiainfo.ocdc.streaming._
 import org.apache.spark.streaming.dstream.DStream
 import scala.xml.Node
 import org.apache.spark.streaming.kafka.KafkaUtils
-import org.apache.spark.SparkConf
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.StreamingContext
 
 class KafkaSource(ssc:StreamingContext) extends StreamingSource(ssc){
 
@@ -15,8 +14,19 @@ class KafkaSource(ssc:StreamingContext) extends StreamingSource(ssc){
     val group = (source \ "groupId").text.toString
     val consumerNum = (source \ "consumerNum").text.toInt
     val separator = (source \ "separator").text.toString
+    val stream_columns = (source \ "stream_columns").text.toString.split(",")
     val topicpMap = topics.split(",").map((_,1)).toMap
     val stream = (1 to consumerNum).map(_=>KafkaUtils.createStream(ssc, zkQuorum, group, topicpMap)).reduce(_.union(_)).map(_._2)
-    stream.map(_.split(separator))
+//    stream.map(_.split(separator))
+
+    // 对输入流列名定义
+    stream.map(x =>{
+      val streamValues = x.split(separator)
+      var result = AnyRef
+      if(stream_columns.size == streamValues.length){
+        result = (0 to streamValues.length-1).map(i=>(stream_columns(i),streamValues(i)))
+      }else throw Exception("流数据配置列名与数据格式不符！")
+      result
+    })
   }
 }
