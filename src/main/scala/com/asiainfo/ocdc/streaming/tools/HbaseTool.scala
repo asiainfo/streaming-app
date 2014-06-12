30d3f649640f5e150fd81c6e5090f1ea4b723f0d
u0jing <u9jing@gmail.com>
2014-06-12 16:04:44 +0800

    HBaseTool封装

---
+++ src/main/scala/com/asiainfo/ocdc/streaming/tools/HbaseTool.scala
@@ -0,0 +1,53 @@
+package com.asiainfo.ocdc.streaming.tools
+
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.{Put, Result, Get, HTable}
+import org.apache.hadoop.hbase.util.Bytes
+import scala.collection.mutable
+
+object HbaseTool {
+
+  val table = new mutable.HashMap[String,HTable]()
+
+  def getTable(tableName:String):HTable={
+
+    table.getOrElse(tableName,{
+      println("----new connection ----")
+      val from = System.currentTimeMillis()
+      val conf = HBaseConfiguration.create()
+      val tbl = new HTable(conf, tableName)
+      val end = System.currentTimeMillis()
+      println("connection time----"+(end-from))
+      table(tableName)= tbl
+      tbl
+    })
+  }
+
+  def getValue(tableName:String,rowKey:String,family:String,qualifiers:Array[String]):Array[(String,String)]={
+    var result:AnyRef = null
+    val table_t =getTable(tableName)
+    val row1 =  new Get(Bytes.toBytes(rowKey))
+    val HBaseRow = table_t.get(row1)
+    if(HBaseRow != null){
+      result = qualifiers.map(c=>{
+        (tableName+"."+c, Bytes.toString(HBaseRow.getValue(Bytes.toBytes(family), Bytes.toBytes(c))))
+      })
+    }
+    else{
+      result=qualifiers.map(c=>{
+        (tableName+"."+c,"")  })
+    }
+    result.asInstanceOf[Array[(String,String)]]
+  }
+
+  def putValue(tableName:String,rowKey:String, family:String,qualifierValue:Array[(String,String)]) {
+    val table =getTable(tableName)
+    val new_row  = new Put(Bytes.toBytes(rowKey))
+    qualifierValue.map(x=>{
+      new_row.add(Bytes.toBytes(family), Bytes.toBytes(x._1), Bytes.toBytes(x._2))
+    })
+
+    table.put(new_row)
+  }
+
+}
