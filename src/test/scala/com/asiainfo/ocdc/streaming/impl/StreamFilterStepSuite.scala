d1df7ccbfc2cc45325f27aee2b184ed897759f67
u0jing <u9jing@gmail.com>
2014-06-17 15:30:35 +0800

    change StreamFilter test

--- src/test/scala/com/asiainfo/ocdc/streaming/impl/StreamFilterStepSuite.scala
+++ src/test/scala/com/asiainfo/ocdc/streaming/impl/StreamFilterStepSuite.scala
@@ -11,9 +11,8 @@ import scala._
  * Created by liuhao8 on 14-6-16.
  */
 class StreamFilterStepSuite extends TestSuitBase with Logging {
-
-  test("DynamicOperateStep test example") {
-      logInfo("DynamicOperateStep test example started ")
+  test("Streamfilter test example1") {
+    logInfo("StreamFilter test example1 started ")
 
     val xmlFile = XML.load("src/test/resources/streamfilter.xml")
     val step = xmlFile \ "step"
@@ -22,26 +21,25 @@ class StreamFilterStepSuite extends TestSuitBase with Logging {
       Seq(Array(("imsi", "460020060188214"),("lac", "0867"),("cell","000B"))),
       Seq(Array(("imsi", "460020060188222"),("lac", "0867"),("cell","0000")))
     )
-
     val expectedOutput = Seq(
       Seq(Array(("imsi", "460020060188214"),("t1.city_id", "2")))
     )
 
     testUtil.createTable("t1","F")
-    val rowValue = (1 to 3).map(i=>("city_id",i.toString)).toArray
-    putValue("t1","460020060188214","F",rowValue)
+    val rowValue = Array(("city_id","2"))
+    putValue("t1","0867000B","F",rowValue)
 
     val filter = new StreamFilter();
     val operation = (s:DStream[Array[ (String, String) ] ]) => filter.onStep(step(0), s)
 
     testOperation(input, operation, expectedOutput, true)
-      logInfo("DynamicOperateStep test example finished ")
+    logInfo("StreamFilter test example1 finished ")
   }
 
 
 
-  test("example2") {
-    logInfo("DynamicOperateStep test example started ")
+  test(" streamFilter test example2") {
+    logInfo("StreamFilter test example2 started ")
 
     val xmlFile = XML.load("src/test/resources/streamfilter.xml")
     val step = xmlFile \ "step"
@@ -56,19 +54,20 @@ class StreamFilterStepSuite extends TestSuitBase with Logging {
     )
 
     testUtil.createTable("t2","F")
-    val rowValue=Array(("city_id","2"),("city_id1","3"))
-    putValue("t1","460020060188214","F",rowValue)
+    val rowValue=Array(("city_id","3"),("city_id1","3"))
+    putValue("t2","460020060188214","F",rowValue)
+    putValue("t2","460020060188222","F",rowValue)
 
     val filter = new StreamFilter();
     val operation = (s:DStream[Array[ (String, String) ] ]) => filter.onStep(step(1), s)
 
     testOperation(input, operation, expectedOutput, true)
-    logInfo("DynamicOperateStep test example finished ")
+    logInfo("StreamFilter test example2 finished ")
   }
 
 
-  test("example3") {
-    logInfo("DynamicOperateStep test example started ")
+  test(" streamFilter test example3") {
+    logInfo("StreamFilter test example3 started ")
 
     val xmlFile = XML.load("src/test/resources/streamfilter.xml")
     val step = xmlFile \ "step"
@@ -84,12 +83,12 @@ class StreamFilterStepSuite extends TestSuitBase with Logging {
 
     testUtil.createTable("t3","F")
     val rowValue=Array(("product_no","215801535555"),("product_id","p1|p2"))
-    putValue("t1","460020060188214","F",rowValue)
+    putValue("t3","460020060188214","F",rowValue)
 
     val filter = new StreamFilter();
-    val operation = (s:DStream[Array[ (String, String) ] ]) => filter.onStep(step(1), s)
+    val operation = (s:DStream[Array[ (String, String) ] ]) => filter.onStep(step(2), s)
 
     testOperation(input, operation, expectedOutput, true)
-    logInfo("DynamicOperateStep test example finished ")
+    logInfo("StreamFilter test example3 finished ")
   }
 }
