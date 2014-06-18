b0dca20a4020a9b7fff9c138ae26bfa794034557
surq <surq@asiainfo.com>
2014-06-18 18:34:04 +0800

    动态数据处理testcase追加

--- src/main/scala/com/asiainfo/ocdc/streaming/impl/DynamicOperate.scala
+++ src/main/scala/com/asiainfo/ocdc/streaming/impl/DynamicOperate.scala
@@ -25,12 +25,16 @@ class DynamicOperate  extends StreamingStep {
     //xml check
     if (!validityCheck(step: Node)) return dstream
 
-    dstream.map(recode => {
+   val tempSream = dstream.map(recode => {
       var imap = recode.toMap
       (imap(key), recode)
     }).groupByKey.map(keyrcode => {
       //　从hbase中取出要累加的初始数据
-      val cellvalue = HbaseTool.getValue(table, keyrcode._1, family, hBaseCells)
+      val getHbaseValue = HbaseTool.getValue(table, keyrcode._1, family, hBaseCells)
+      // 如果hbase中无基础数据时，把null转换为“0”
+      var cellvalue = Map[String, String]()
+      getHbaseValue.foreach(f=>{if ((f._2).toLowerCase()=="null")cellvalue +=(f._1 -> "0") else  cellvalue +=(f._1 -> f._2)})
+      
       // 要更新的operaters与hbase.cell一一对应
       val cellexp = hBaseCells.zip(operaters)
 
@@ -50,6 +54,14 @@ class DynamicOperate  extends StreamingStep {
       HbaseTool.putValue(table, keyrcode._1, family, mapSet.toArray)
       keyrcode
     }).flatMap(_._2)
+    
+        // 结果输出
+    val result = tempSream.map(x => {
+      //如果input output相同的字段完全相同，说明不需要规整数据，不做map
+      val item = x.toMap
+      (0 to output.length - 1).map(i => (output(i), item.getOrElse(output(i), output(i)))).toArray
+    })
+    result
   }
   
   /**
--- src/test/scala/com/asiainfo/ocdc/streaming/impl/DynamicOperateStepSuite.scala
+++ src/test/scala/com/asiainfo/ocdc/streaming/impl/DynamicOperateStepSuite.scala
@@ -15,47 +15,190 @@ import org.apache.spark.streaming.dstream.DStream
  */
 class DynamicOperateStepSuite extends TestSuitBase with Logging {
 
-  test("DynamicOperate test example") {
-    logInfo("DynamicOperateStep test example started ")
+  logInfo("DynamicOperateStep test example started ")
+  step0(0, "为hbase各rowkey设定初始基值的情况下计算流量，话费同时累加计算，并且筛选输入流字段做为输出")
+  step0_1(0, "hbase为初始状态没有任何数据的情况下计算流量，话费同时累加计算，并且筛选输入流字段做为输出")
+  step1(1, "把输入的全部字段输出(附带累加计算)")
+  step2(2, "把流中的记录的个别字段更新到hbase(hbase初始有基础数据)")
+  
+  logInfo("DynamicOperate test example finished ")
 
-    val xmlFile = XML.load("src/test/resources/DynamicOperate-test.xml")
-    val step = xmlFile \ "step"
-    val family = "F"
-    val table = (step \ "HBaseTable").text.toString.trim
+  /**
+   *  为hbase各rowkey设定初始基值的情况下计算流量，话费同时累加计算，并且筛选输入流字段做为输出
+   */
+  def step0(stepNum: Int, caseNmae: String) {
+    test(caseNmae) {
 
-    testUtil.createTable(table, family)
-    putValue(table, "18600640175", family, Array.apply(("NTcount", "100"), ("Fee", "1000")))
-    putValue(table, "18600640176", family, Array.apply(("NTcount", "100"), ("Fee", "1000")))
-    putValue(table, "18600640177", family, Array.apply(("NTcount", "100"), ("Fee", "1000")))
+      logInfo("DynamicOperateStep-" + caseNmae + " test example start... ")
 
-    val input = Seq(
-      Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "20"), ("fee", "10"))),
-      Seq(Array(("TELNo", "18600640176"), ("name", "asia"), ("sex", "man"), ("netcount", "33"), ("fee", "11"))),
-      Seq(Array(("TELNo", "18600640177"), ("name", "asia1"), ("sex", "man"), ("netcount", "31"), ("fee", "1"))),
-      Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "30"), ("fee", "20"))),
-      Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "40"), ("fee", "31"))))
+      val xmlFile = XML.load("src/test/resources/DynamicOperate-test.xml")
+      val steps = xmlFile \ "step"
+      val step = steps(stepNum)
+      val family = "F"
+      val table = (step \ "HBaseTable").text.toString.trim
 
+      testUtil.createTable(table, family)
+      putValue(table, "18600640175", family, Array.apply(("NTcount", "100"), ("Fee", "1000")))
+      putValue(table, "18600640176", family, Array.apply(("NTcount", "100"), ("Fee", "1000")))
+      putValue(table, "18600640177", family, Array.apply(("NTcount", "100"), ("Fee", "1000")))
 
-    val expectedOutput = Seq(Seq(Array.apply((table+".NTcount","190"),(table+".Fee","1061"))), Seq(Array.apply((table+".NTcount","133"),(table+".Fee","1011"))), Seq(Array.apply((table+".NTcount","131"),(table+".Fee","1001"))))
-    val filter = new DynamicOperate();
-    val operation = (s: DStream[Array[(String, String)]]) => filter.onStep(step(0), s)
-    
-    testOperation(input, operation, input, true)
-    
-    val result75 = getValue(table, "18600640175", family, Array.apply("NTcount", "Fee"))
-    val result76 = getValue(table, "18600640176", family, Array.apply("NTcount", "Fee"))
-    val result77 = getValue(table, "18600640177", family, Array.apply("NTcount", "Fee"))
-    val resultset = Seq(Seq(result75), Seq(result76), Seq(result77))
-    
-//      resultset.foreach(f=>{
-//        f.foreach(x=>{
-//          x.foreach(k=>println("=====result============"+ k._1+":"+k._2))
-//        })
-//      })
-      
-    verifyOutput(resultset,expectedOutput, true)
+      val input = Seq(
+        Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "20"), ("fee", "10"))),
+        Seq(Array(("TELNo", "18600640176"), ("name", "asia"), ("sex", "man"), ("netcount", "33"), ("fee", "11"))),
+        Seq(Array(("TELNo", "18600640177"), ("name", "asia1"), ("sex", "man"), ("netcount", "31"), ("fee", "1"))),
+        Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "30"), ("fee", "20"))),
+        Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "40"), ("fee", "31"))))
 
-    logInfo("DynamicOperate test example finished ")
+      val expectedHbase = Seq(Seq(Array.apply((table + ".NTcount", "190"), (table + ".Fee", "1061"))), Seq(Array.apply((table + ".NTcount", "133"), (table + ".Fee", "1011"))), Seq(Array.apply((table + ".NTcount", "131"), (table + ".Fee", "1001"))))
+      val expectedOutput = Seq(Seq(Array.apply(("TELNo", "18600640175"), ("name", "surq"))), Seq(Array.apply(("TELNo", "18600640176"), ("name", "asia"))), Seq(Array.apply(("TELNo", "18600640177"), ("name", "asia1"))), Seq(Array.apply(("TELNo", "18600640175"), ("name", "surq"))), Seq(Array.apply(("TELNo", "18600640175"), ("name", "surq"))))
+
+      val filter = new DynamicOperate();
+      val operation = (s: DStream[Array[(String, String)]]) => filter.onStep(step, s)
+
+      testOperation(input, operation, expectedOutput, true)
+
+      val result75 = getValue(table, "18600640175", family, Array.apply("NTcount", "Fee"))
+      val result76 = getValue(table, "18600640176", family, Array.apply("NTcount", "Fee"))
+      val result77 = getValue(table, "18600640177", family, Array.apply("NTcount", "Fee"))
+      val resultset = Seq(Seq(result75), Seq(result76), Seq(result77))
+      //      resultset.foreach(f=>{
+      //        f.foreach(x=>{
+      //          x.foreach(k=>println("=====result============"+ k._1+":"+k._2))
+      //        })
+      //      })
+
+      verifyOutput(resultset, expectedHbase, true)
+      logInfo("DynamicOperateStep-" + caseNmae + " test example completion!")
+    }
   }
 
+  /**
+   * hbase为初始状态没有任何数据的情况下计算流量，话费同时累加计算，并且筛选输入流字段做为输出
+   */
+  def step0_1(stepNum: Int, caseNmae: String) {
+    test(caseNmae) {
+
+      logInfo("DynamicOperateStep-" + caseNmae + " test example start... ")
+
+      val xmlFile = XML.load("src/test/resources/DynamicOperate-test.xml")
+      val step = (xmlFile \ "step")(stepNum)
+      val family = "F"
+
+      val table = (step \ "HBaseTable").text.toString.trim
+
+      testUtil.createTable(table, family)
+
+      val input = Seq(
+        Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "20"), ("fee", "10"))),
+        Seq(Array(("TELNo", "18600640176"), ("name", "asia"), ("sex", "man"), ("netcount", "33"), ("fee", "11"))),
+        Seq(Array(("TELNo", "18600640177"), ("name", "asia1"), ("sex", "man"), ("netcount", "31"), ("fee", "1"))),
+        Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "30"), ("fee", "20"))),
+        Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "40"), ("fee", "31"))))
+
+      val expectedHbase = Seq(Seq(Array.apply((table + ".NTcount", "90"), (table + ".Fee", "61"))), Seq(Array.apply((table + ".NTcount", "33"), (table + ".Fee", "11"))), Seq(Array.apply((table + ".NTcount", "31"), (table + ".Fee", "1"))))
+      val expectedOutput = Seq(Seq(Array.apply(("TELNo", "18600640175"), ("name", "surq"))), Seq(Array.apply(("TELNo", "18600640176"), ("name", "asia"))), Seq(Array.apply(("TELNo", "18600640177"), ("name", "asia1"))), Seq(Array.apply(("TELNo", "18600640175"), ("name", "surq"))), Seq(Array.apply(("TELNo", "18600640175"), ("name", "surq"))))
+
+      val filter = new DynamicOperate();
+      val operation = (s: DStream[Array[(String, String)]]) => filter.onStep(step, s)
+
+      testOperation(input, operation, expectedOutput, true)
+
+      val result75 = getValue(table, "18600640175", family, Array.apply("NTcount", "Fee"))
+      val result76 = getValue(table, "18600640176", family, Array.apply("NTcount", "Fee"))
+      val result77 = getValue(table, "18600640177", family, Array.apply("NTcount", "Fee"))
+      val resultset = Seq(Seq(result75), Seq(result76), Seq(result77))
+
+      verifyOutput(resultset, expectedHbase, true)
+      logInfo("DynamicOperateStep-" + caseNmae + " test example completion!")
+    }
+  }
+
+  /**
+   * 把输入的全部字段输出(附带累加计算)
+   */
+  def step1(stepNum: Int, caseNmae: String) {
+
+    test(caseNmae) {
+
+      logInfo("DynamicOperateStep-" + caseNmae + " test example start... ")
+
+      val xmlFile = XML.load("src/test/resources/DynamicOperate-test.xml")
+      val step = (xmlFile \ "step")(stepNum)
+      val family = "F"
+
+      val table = (step \ "HBaseTable").text.toString.trim
+
+      testUtil.createTable(table, family)
+      putValue(table, "18600640175", family, Array.apply(("NTcount", "100"), ("Fee", "1000")))
+      putValue(table, "18600640176", family, Array.apply(("NTcount", "100"), ("Fee", "1000")))
+      putValue(table, "18600640177", family, Array.apply(("NTcount", "100"), ("Fee", "1000")))
+
+      val input = Seq(
+        Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "20"), ("fee", "10"))),
+        Seq(Array(("TELNo", "18600640176"), ("name", "asia"), ("sex", "man"), ("netcount", "33"), ("fee", "11"))),
+        Seq(Array(("TELNo", "18600640177"), ("name", "asia1"), ("sex", "man"), ("netcount", "31"), ("fee", "1"))),
+        Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "30"), ("fee", "20"))),
+        Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "40"), ("fee", "31"))))
+
+      val expectedHbase = Seq(Seq(Array.apply((table + ".NTcount", "190"), (table + ".Fee", "1061"))), Seq(Array.apply((table + ".NTcount", "133"), (table + ".Fee", "1011"))), Seq(Array.apply((table + ".NTcount", "131"), (table + ".Fee", "1001"))))
+
+      val filter = new DynamicOperate();
+      val operation = (s: DStream[Array[(String, String)]]) => filter.onStep(step, s)
+
+      // 输入字段全部输出
+      testOperation(input, operation, input, true)
+
+      val result75 = getValue(table, "18600640175", family, Array.apply("NTcount", "Fee"))
+      val result76 = getValue(table, "18600640176", family, Array.apply("NTcount", "Fee"))
+      val result77 = getValue(table, "18600640177", family, Array.apply("NTcount", "Fee"))
+      val resultset = Seq(Seq(result75), Seq(result76), Seq(result77))
+
+      verifyOutput(resultset, expectedHbase, true)
+      logInfo("DynamicOperateStep-" + caseNmae + " test example completion!")
+    }
+  }
+  
+  /**
+   * 把流中的记录的个别字段更新到hbase
+   */
+  def step2(stepNum: Int, caseNmae: String) {
+
+    test(caseNmae) {
+
+      logInfo("DynamicOperateStep-" + caseNmae + " test example start... ")
+
+      val xmlFile = XML.load("src/test/resources/DynamicOperate-test.xml")
+      val step = (xmlFile \ "step")(stepNum)
+      val family = "F"
+
+      val table = (step \ "HBaseTable").text.toString.trim
+
+      testUtil.createTable(table, family)
+      putValue(table, "18600640175", family, Array.apply(("NTcount", "100"), ("Fee", "1000")))
+      putValue(table, "18600640176", family, Array.apply(("NTcount", "100"), ("Fee", "1000")))
+      putValue(table, "18600640177", family, Array.apply(("NTcount", "100"), ("Fee", "1000")))
+
+      val input = Seq(
+        Seq(Array(("TELNo", "18600640175"), ("name", "surq"), ("sex", "man"), ("netcount", "20"), ("fee", "10"))),
+        Seq(Array(("TELNo", "18600640176"), ("name", "asia"), ("sex", "man"), ("netcount", "33"), ("fee", "11"))),
+        Seq(Array(("TELNo", "18600640177"), ("name", "asia1"), ("sex", "man"), ("netcount", "31"), ("fee", "1"))))
+
+      val expectedHbase = Seq(Seq(Array.apply((table + ".NTcount", "20"), (table + ".Fee", "10"))), Seq(Array.apply((table + ".NTcount", "33"), (table + ".Fee", "11"))), Seq(Array.apply((table + ".NTcount", "31"), (table + ".Fee", "1"))))
+
+      val filter = new DynamicOperate();
+      val operation = (s: DStream[Array[(String, String)]]) => filter.onStep(step, s)
+
+      // 输入字段全部输出
+      testOperation(input, operation, input, true)
+
+      val result75 = getValue(table, "18600640175", family, Array.apply("NTcount", "Fee"))
+      val result76 = getValue(table, "18600640176", family, Array.apply("NTcount", "Fee"))
+      val result77 = getValue(table, "18600640177", family, Array.apply("NTcount", "Fee"))
+      val resultset = Seq(Seq(result75), Seq(result76), Seq(result77))
+
+      verifyOutput(resultset, expectedHbase, true)
+      logInfo("DynamicOperateStep-" + caseNmae + " test example completion!")
+    }
+  }
+  
 }
