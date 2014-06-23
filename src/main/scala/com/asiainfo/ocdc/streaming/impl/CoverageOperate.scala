b382726193a3870122127a297f004f87500153b9
surq <surq@asiainfo.com>
2014-06-23 11:35:51 +0800

    added by @luogankun: CoverageOperate.scala CoverageOperate-test.xml

---
+++ src/main/scala/com/asiainfo/ocdc/streaming/impl/CoverageOperate.scala
@@ -0,0 +1,33 @@
+package com.asiainfo.ocdc.streaming.impl
+
+import com.asiainfo.ocdc.streaming._
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.hadoop.hbase.util.Bytes
+import scala.xml.Node
+import com.asiainfo.ocdc.streaming.tools._
+
+class CoverageOperate extends StreamingStep {
+
+  def onStep(step: Node, inStream: DStream[Array[(String, String)]]): DStream[Array[(String, String)]] = {
+
+    val result = inStream
+
+    val table = (step \ "HBaseTable").text.toString.trim
+    val key = (step \ "HBaseKey").text.toString.trim
+    val hBaseCells = (step \ "HBaseCells").text.toString.trim.split(",")
+    val expressions = (step \ "expressions").text.toString.trim.split(",")
+    val output = (step \ "output").text.toString.trim.split(",")
+
+    val handle = inStream.map(record => {
+      var recordMap = record.toMap
+      (recordMap(key), record)
+
+      val expeValue = for { index <- 0 until expressions.size } yield (JexlTool.getExpValue(expressions(index), recordMap.toArray))
+      val cellexp = hBaseCells.zip(expressions)
+      HbaseTool.putValue(table, recordMap(key), HbaseTool.family, cellexp)
+    })
+    
+    result
+  }
+
+}
