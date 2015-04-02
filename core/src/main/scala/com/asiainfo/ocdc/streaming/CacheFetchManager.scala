9589f522466e1ecad27f82f5739bd3046c5a57c9
maji2014 <maji3@asiainfo.com>
2015-04-02 00:38:04 -0700

    add CacheFetchManager class

---
+++ core/src/main/scala/com/asiainfo/ocdc/streaming/CacheFetchManager.scala
@@ -0,0 +1,13 @@
+package com.asiainfo.ocdc.streaming
+
+class CacheFetchManager {
+  def factory(manager: String):CacheManager = {
+    if(manager.equals("TextCacheManager")){
+      TextCacheManager
+    }else if(manager.equals("CodisCacheManager")){
+      CodisCacheManager
+    }else{
+      throw new RuntimeException("CacheFetchManager is not found!")
+    }
+  }
+}
