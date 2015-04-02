64bfda3064876e6fe2d348a27a04bc5eae020138
maji2014 <maji3@asiainfo.com>
2015-04-02 03:11:08 -0700

    update TextCacheManager

--- core/src/main/scala/com/asiainfo/ocdc/streaming/TextCacheManager.scala
+++ core/src/main/scala/com/asiainfo/ocdc/streaming/TextCacheManager.scala
@@ -1,16 +1,49 @@
 package com.asiainfo.ocdc.streaming
-
+import scala.io.Source
 
 
 object TextCacheManager extends CacheManager {
 
   private val CommonCacheMap:Map[String, Map[String, String]] = Map("LGMAP"-> Map("laccell" -> "2015map"))
   private val CommonCacheValue:Map[String,Map[String, String]] = Map("LGValue"-> Map("laccell" -> "2015value"))
-  private val CommonCacheList:Map[String,List[String]] = Map("LGList" -> List("2015list"))
+  private val CommonCacheList = Map[String,List[String]]()
 
   private var HashCacheMap = Map[String, Map[String, String]]()
   private var HashCacheString = Map[String,String]()
   private var HashCacheList = Map[String,List[String]]()
+
+  def init(){
+    CommonCacheMapinit("/home/ocdc/asiainfo/cachemap")
+    CommonCacheValueinit
+    CommonCacheListinit
+  }
+
+ // getCommonCacheList(key: String): List[String]
+ // key1, value11,value12,value13,value14,value15
+ // key2, value21,value22,value23,value24,value25
+
+  def CommonCacheListinit(filename: String) {
+    for (line <- Source.fromFile(filename).getLines){
+      val kv = line.split(",")
+      CommonCacheList += (kv(0) -> kv.toList)
+    }
+  }
+
+//  getCommonCacheMap(key: String): Map[String, String]
+//  KeyA,keya1,valuea1,keya2,valuea2,keya3,valuea3
+//  KeyB,keyb1,valueb1,keyb2,valueb2,keyb3,valueb3
+
+  def CommonCacheMapinit() {
+
+  }
+
+  //getCommonCacheValue(cacheName: String, key: String): String
+  //groupA,lacAcellA,AAAA
+  //groupA,lacBcellB,BBBB
+
+  def CommonCacheValueinit() {
+
+  }
 
 
   override def getHashCacheList(key: String): List[String] = {
