610802641cebf0258e23bb44c3426e3357809be6
leo <liuhao8@asiainfo.com>
2015-04-03 15:05:01 +0800

    add drop table script

--- bin/CreateTableScript.sql
+++ bin/CreateTableScript.sql
@@ -1,4 +1,5 @@
 -- 1 MainFrameProp
+DROP TABLE IF EXISTS `MainFrameProp`;
 CREATE TABLE `MainFrameProp` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
@@ -8,6 +9,7 @@ CREATE TABLE `MainFrameProp` (
 
 
 -- 2 Kafka Sources
+DROP TABLE IF EXISTS `KafkaSource`;
 CREATE TABLE `KafkaSource` (
   `id` int NOT NULL AUTO_INCREMENT,
   `topic` varchar(30) NOT NULL,
@@ -21,8 +23,15 @@ CREATE TABLE `KafkaSource` (
 );
 
 -- 3 HDFS Sources
+DROP TABLE IF EXISTS `HDFSSource`;
+CREATE TABLE `HDFSSource` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `path` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
 -- 4 EventSources
+DROP TABLE IF EXISTS `EventSource`;
 CREATE TABLE `EventSource` (
   `id` int NOT NULL AUTO_INCREMENT,
   `type` varchar(10) NOT NULL,
@@ -36,6 +45,7 @@ alter table EventSource add foreign key (sourceid) references KafkaSource(id) ON
 DELETE CASCADE;
 
 -- 5 LabelRules
+DROP TABLE IF EXISTS `LabelRules`;
 CREATE TABLE `LabelRules` (
   `id` int NOT NULL AUTO_INCREMENT,
   `esourceid` int NOT NULL,
@@ -46,6 +56,7 @@ alter table LabelRules add foreign key (esourceid) references EventSource(id) ON
 DELETE CASCADE;
 
 -- 6 LabelRulesProp
+DROP TABLE IF EXISTS `LabelRulesProp`;
 CREATE TABLE `LabelRulesProp` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
@@ -57,6 +68,7 @@ alter table LabelRulesProp add foreign key (lrid) references LabelRules(id) ON
 DELETE CASCADE;
 
 -- 7 EventRules
+DROP TABLE IF EXISTS `EventRules`;
 CREATE TABLE `EventRules` (
   `id` int NOT NULL AUTO_INCREMENT,
   `esourceid` int NOT NULL,
@@ -67,6 +79,7 @@ alter table EventRules add foreign key (esourceid) references EventSource(id) ON
 DELETE CASCADE;
 
 -- 8 EventRulesProp
+DROP TABLE IF EXISTS `EventRulesProp`;
 CREATE TABLE `EventRulesProp` (
   `id` int NOT NULL,
   `name` varchar(30) NOT NULL,
