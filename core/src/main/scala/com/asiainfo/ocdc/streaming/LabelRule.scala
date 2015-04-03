28959882b829660739557c9e28d79ce8d2e768df
leo <liuhao8@asiainfo.com>
2015-04-03 11:53:12 +0800

    modify init LabelRule and EventRule config

--- core/src/main/scala/com/asiainfo/ocdc/streaming/LabelRule.scala
+++ core/src/main/scala/com/asiainfo/ocdc/streaming/LabelRule.scala
@@ -3,11 +3,11 @@ package com.asiainfo.ocdc.streaming
 /**
  * Created by tianyi on 3/26/15.
  */
-trait LabelRule extends Serializable{
+trait LabelRule extends Serializable {
   // load config from LabelRuleConf
   var conf: LabelRuleConf
 
-  def init(lrconf: LabelRuleConf){
+  def init(lrconf: LabelRuleConf) {
     conf = lrconf
   }
 
