4ebc5cd922c61bced8b55a2d71e6306c8c1d4d7f
tsingfu <happyyangfq@gmail.com>
2015-04-08 11:12:29 +0800

    更新 LabelRuleConf 的单元测试，测试通过

---
+++ core/src/test/scala/com/asiainfo/ocdc/streaming/LabelRuleConfSuite.scala
@@ -0,0 +1,29 @@
+package com.asiainfo.ocdc.streaming
+
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+import scala.collection.immutable
+
+/**
+ * Created by yfq on 15/4/3.
+ */
+class LabelRuleConfSuite extends FunSuite with BeforeAndAfter {
+
+	test("1 测试 LabelRule 配置的获取") {
+
+		val map = immutable.Map[String, String]()
+		val lrConf = new LabelRuleConf(map)
+		lrConf.setAll(map)
+		lrConf.set("classname","com.asiainfo.ocdc.streaming.LocationStayRule")
+		lrConf.set("stay.limits", (20 * 60 * 1000).toString)
+		lrConf.set("stay.matchMax", "true")
+		lrConf.set("stay.outputThreshold", "true")
+		lrConf.set("stay.timeout", (30 * 60 * 1000).toString)
+
+		assert(lrConf.getClassName()=="com.asiainfo.ocdc.streaming.LocationStayRule")
+		assert(lrConf.get("stay.limits") == 20 * 60 * 1000 +"")
+		assert(lrConf.get("stay.matchMax")=="true")
+		assert(lrConf.get("stay.outputThreshold")=="true")
+		assert(lrConf.get("stay.timeout")==30 * 60 * 1000 +"")
+	}
+}
