9fa8701fb8bee11c6696e61bb1da0f021c7b2a22
leo <liuhao8@asiainfo.com>
2015-04-09 10:07:50 +0800

    half modify

--- event/mc-signal/src/main/scala/com/asiainfo/ocdc/streaming/MCEventSource.scala
+++ event/mc-signal/src/main/scala/com/asiainfo/ocdc/streaming/MCEventSource.scala
@@ -1,12 +1,15 @@
 package com.asiainfo.ocdc.streaming
 
+import java.text.SimpleDateFormat
+
 class MCEventSource() extends EventSource() {
 
   def formatSource(inputs: Array[String]): Option[MCSourceObject] = {
     // 事件ID,时间,LAC,CI,主叫IMEI,被叫IMEI,主叫IMSI,被叫IMSI
     try {
+      val sdf = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss")
       val eventID = inputs(0).toInt
-      val time = inputs(1).toLong
+      val time = sdf.parse(inputs(1)).getTime
       val lac = inputs(2).toInt
       val ci = inputs(3).toInt
       val imei = inputs(4).toInt
@@ -20,8 +23,8 @@ class MCEventSource() extends EventSource() {
   }
 
   override def transform(source: String): Option[MCSourceObject] = {
-    val inputArray = source.split(conf.get("source.format.delim"))
-    if(source.length != conf.getInt("source.format.length")) {
+    val inputArray = source.split(conf.get("delim"))
+    if(inputArray.length != conf.getInt("formatlength")) {
       None
     } else {
       formatSource(inputArray)
