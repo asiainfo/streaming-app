a958f8e82bd50f7f6a3a5f25e8cb8b7ed7c15730
tsingfu <happyyangfq@gmail.com>
2015-04-13 20:01:25 +0800

    1 编写加载MC日志到redis的工具类，发现新问题：CacheManager.setCommonCacheValue 调用时在 getConnect 处挂起

---
+++ core/src/test/scala/com/asiainfo/ocdc/streaming/tool/areaMapping/LoadFile2RedisSuite.scala
@@ -0,0 +1,29 @@
+package com.asiainfo.ocdc.streaming.tool.areaMapping
+
+import com.asiainfo.ocdc.streaming.{CacheFactory, MainFrameConf}
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+/**
+ * Created by tsingfu on 15/4/13.
+ */
+class LoadFile2RedisSuite extends FunSuite with BeforeAndAfter {
+
+	//test with CodisCacherManager
+	MainFrameConf.set("DefaultCacheManager", "CodisCacheManager")
+	MainFrameConf.set("CodisProxy","redis1:6379")
+	MainFrameConf.set("JedisMEM","10000")
+	MainFrameConf.set("JedisMaxActive","100")
+	MainFrameConf.set("JedisMaxActive","15")
+	val cacheMgr = CacheFactory.getManager
+
+	test("test load area mapping into redis from mc log"){
+		val fileName="/data01/data/datadir_github/ai-projects/streaming-app-201503/testdata/mc-test.log"
+		val serverPort="redis1:6379"
+		val mapKey="lacci2area_test1"
+
+//		LoadFile2Redis.load(fileName, serverPort, mapKey)
+		LoadFile2Redis.load2(fileName, serverPort, mapKey)
+//		assert(cacheMgr.getCommonCacheMap(mapKey).size==10)
+
+	}
+}
