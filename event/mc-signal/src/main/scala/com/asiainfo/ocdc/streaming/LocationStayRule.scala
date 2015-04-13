e91a72f6d1e4c2a7beb751612eab54352f711fba
surq <surq@asiainfo.com>
2015-04-13 17:04:29 +0800

    review 修正

--- event/mc-signal/src/main/scala/com/asiainfo/ocdc/streaming/LocationStayRule.scala
+++ event/mc-signal/src/main/scala/com/asiainfo/ocdc/streaming/LocationStayRule.scala
@@ -1,6 +1,6 @@
 package com.asiainfo.ocdc.streaming
 
-import scala.collection.mutable.{Map, ArrayBuffer}
+import scala.collection.mutable.{ Map, ArrayBuffer }
 
 /**
  * @author surq
@@ -9,14 +9,14 @@ import scala.collection.mutable.{Map, ArrayBuffer}
  */
 
 class LabelProps extends StreamingCache with Serializable {
-  var labelsPropList:Map[String,Map[String,String]] = Map[String,Map[String,String]]()
+  var labelsPropList: Map[String, Map[String, String]] = Map[String, Map[String, String]]()
 }
 
 class LocationStayRule extends MCLabelRule {
   // TODO 配置文件读入的，
-//  val selfDefStayTimeList = Array(10 * 60 * 1000, 5 * 60 * 1000, 3 * 60 * 1000).sorted
-	lazy val selfDefStayTimeList = conf.get("stay.limits").split(",")
-  // 推送满足设置的数据坎的最小值:true;最大值：false
+  //  val selfDefStayTimeList = Array(10 * 60 * 1000, 5 * 60 * 1000, 3 * 60 * 1000).sorted
+  lazy val selfDefStayTimeList = conf.get("stay.limits").split(",")
+  // 推送满足设置的数据坎的最大值:true;最小值：false
   lazy val userDefPushOrde = conf.getBoolean("stay.matchMax", true)
   // 推送满足设置的数据的限定值，还是真实的累计值.真实的累计值:false;限定值:true
   lazy val pushLimitValue = conf.getBoolean("stay.outputThreshold", true)
@@ -24,56 +24,47 @@ class LocationStayRule extends MCLabelRule {
   lazy val thresholdValue = conf.getLong("stay.timeout", 30 * 60 * 1000)
 
   def evaluateTime(oldStayTime: Long, newStayTime: Long): Long = {
-		if(newStayTime <= oldStayTime){
-			0
-		} else {
-			val matchList = selfDefStayTimeList.filter(limit => (oldStayTime <= limit.toLong && newStayTime >= limit.toLong))
-		  if(matchList.isEmpty) {
-        0
-      } else {
-        matchList.map(_.toLong).max
-      }
+
+    val matchList = selfDefStayTimeList.filter(limit => (oldStayTime <= limit.toLong && newStayTime >= limit.toLong))
+    if (matchList.isEmpty) {
+      0
+    } else {
+      if (userDefPushOrde) matchList.map(_.toLong).max else matchList.map(_.toLong).min
     }
   }
 
   def attachMCLabel(mc: MCSourceObject, cache: StreamingCache) {
-    var cacheInstance: LabelProps = null
-    if(cache == null) cacheInstance = new LabelProps
-    else cacheInstance = cache.asInstanceOf[LabelProps]
+    val cacheInstance = if (cache == null) new LabelProps
+    else cache.asInstanceOf[LabelProps]
 
-		if(cacheInstance.labelsPropList == null){
-			cacheInstance.labelsPropList = Map[String,Map[String,String]]()
-		}
-
-		// 取在siteRule（区域规则）中所打的area标签list
+    // 取在siteRule（区域规则）中所打的area标签list
     val locationList = (mc.getLabels(Constant.LABEL_ONSITE)).keys
 
     // mcsource labels用
     val mcStayLabelsMap = Map[String, String]()
 
     // 使用宽松的过滤策略，相同区域信令如果间隔超过${thresholdValue}，则判定为不连续
-    locationList.map( location => {
-
-      cacheInstance.labelsPropList.get(location) match {
+    locationList.map(location => {
+      val area = cacheInstance.labelsPropList.get(location)
+      area match {
         case None => {
           mcStayLabelsMap += (location -> "0")
-          cacheInstance.labelsPropList.put (location, Map(
+          cacheInstance.labelsPropList.put(location, Map(
             Constant.LABEL_STAY_FIRSTTIME -> (mc.time).toString,
             Constant.LABEL_STAY_LASTTIME -> (mc.time).toString))
-
         }
         case Some(currentStatus) => {
           val first = currentStatus.get(Constant.LABEL_STAY_FIRSTTIME).getOrElse("0").toLong
           val last = currentStatus.get(Constant.LABEL_STAY_LASTTIME).getOrElse("0").toLong
 
-          if(first > last) {
+          if (first > last) {
             // 无效数据，丢弃，本条视为first
             mcStayLabelsMap += (location -> "0")
-            cacheInstance.labelsPropList.put (location, Map(
+            cacheInstance.labelsPropList.put(location, Map(
               Constant.LABEL_STAY_FIRSTTIME -> (mc.time).toString,
               Constant.LABEL_STAY_LASTTIME -> (mc.time).toString))
-          } else if(mc.time < first) {
-            if(first - mc.time > thresholdValue) {
+          } else if (mc.time < first) {
+            if (first - mc.time > thresholdValue) {
               // 本条记录无效，输出空标签，不更新cache
               mcStayLabelsMap += (location -> "0")
             } else {
@@ -81,13 +72,13 @@ class LocationStayRule extends MCLabelRule {
               currentStatus.put(Constant.LABEL_STAY_FIRSTTIME, mc.time.toString)
               mcStayLabelsMap.put(location, evaluateTime(last - first, last - mc.time).toString)
             }
-          } else if(mc.time <= last){
+          } else if (mc.time <= last) {
             // 本条属于延迟到达，不处理
             mcStayLabelsMap += (location -> "0")
-          } else if(mc.time - last> thresholdValue) {
+          } else if (mc.time - last > thresholdValue) {
             // 本条与上一条数据间隔过大，判定为不连续
             mcStayLabelsMap += (location -> "0")
-            cacheInstance.labelsPropList.put (location, Map(
+            cacheInstance.labelsPropList.put(location, Map(
               Constant.LABEL_STAY_FIRSTTIME -> (mc.time).toString,
               Constant.LABEL_STAY_LASTTIME -> (mc.time).toString))
           } else {
@@ -95,7 +86,6 @@ class LocationStayRule extends MCLabelRule {
             currentStatus.put(Constant.LABEL_STAY_LASTTIME, mc.time.toString)
             mcStayLabelsMap.put(location, evaluateTime(last - first, mc.time - first).toString)
           }
-
         }
       }
     })
@@ -104,4 +94,3 @@ class LocationStayRule extends MCLabelRule {
     mc.setLabel(Constant.LABEL_STAY, mcStayLabelsMap)
   }
 }
-
