a8c39d3fbe30af01ef5917785bd2e076abc46e39
tsingfu <happyyangfq@gmail.com>
2015-07-29 11:41:10 +0800

    1 Hgetall.scala, ScanHashKeyAndHgetall.scala: 添加配置函数

--- tools/src/main/scala/tools/redis/export/ScanHashKeyAndHgetall.scala
+++ tools/src/main/scala/tools/redis/export/ScanHashKeyAndHgetall.scala
@@ -1,8 +1,11 @@
 package tools.redis.export
 
+import java.util.Properties
+
 import org.slf4j.LoggerFactory
-import tools.redis.FindMultiImsi2OneProduct.init_props_fromXml
 import tools.redis.RedisUtils
+
+import scala.xml.XML
 
 /**
  * Created by tsingfu on 15/7/25.
@@ -44,4 +47,43 @@ object ScanHashKeyAndHgetall extends App {
     jedis.close()
   }
 
+  /**
+   * 从xml文件中初始化配置
+   * @param confXmlFile
+   * @return
+   */
+  def init_props_fromXml(confXmlFile: String): Properties ={
+
+    val props = new Properties()
+
+    val conf = XML.load(confXmlFile)
+
+    val servers = (conf \ "redis" \ "servers").text.trim
+    val database = (conf \ "redis" \ "database").text.trim
+    val timeout = (conf \ "redis" \ "timeout").text.trim
+    val passwd = (conf \ "redis" \ "password").text.trim
+    val password = if (passwd == "" || passwd == null) null else passwd
+
+    props.put("redis.servers", servers)
+    props.put("redis.database", database)
+    props.put("redis.timeout", timeout)
+
+    if(password != null || password == "") props.put("redis.password", password)
+
+    val maxTotal = (conf \ "jedisPool" \ "maxTotal").text.trim
+    val maxIdle = (conf \ "jedisPool" \ "maxIdle").text.trim
+    val minIdle = (conf \ "jedisPool" \ "minIdle").text.trim
+
+    props.put("jedisPool.maxTotal", maxTotal)
+    props.put("jedisPool.maxIdle", maxIdle)
+    props.put("jedisPool.minIdle", minIdle)
+
+
+    println("="*80)
+    //TODO: 解决properties配置项输出乱序问题
+    props.list(System.out)//问题，没有顺序
+    println("="*80)
+
+    props
+  }
 }
